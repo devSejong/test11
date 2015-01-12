d3d1cbfe7fda612c42288ba1debe6f9f80475595
SejongPark <devsejong@gmail.com>
2015-01-12 15:02:03 +0900

    .

--- si-template-projects/adapter/src/main/java/sipackage/config/xml/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides parser classes to provide Xml namespace support for the SIAdapterUpperPrefix components.
- */
-package sipackage.config.xml;
--- si-template-projects/adapter/src/main/java/sipackage/core/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides core classes of the SIAdapterUpperPrefix module.
- */
-package sipackage.core;
